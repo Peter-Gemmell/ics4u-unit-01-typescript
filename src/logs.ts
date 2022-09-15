99a2639fe2b3edf3712468f39143873e863ea5d2
Peter Gemmell <peter.gemmell@mths.ca~>
2022-09-15 13:29:48 +0000

    night

--- src/logs.ts
+++ src/logs.ts
@@ -11,11 +11,12 @@ import promptSync from 'prompt-sync'
 
 const prompt = promptSync()
 // let logSize: number
-
+const logWeight = 20
+const truckWeight = 1100
 const logString = prompt('What is the size of the log? (.25m, .5m, 1m): ')
 const logSize = parseFloat(logString)
-const weight = logSize * 20
-const logAmount = 1100 / weight
+const weight = logSize * logWeight
+const logAmount = truckWeight / weight
 console.log(` ${logAmount} ${logSize} logs would fill the truck`)
 
 console.log('\nDone.')
